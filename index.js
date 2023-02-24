e27dc1e55d0551b2582efb9cb880a29aff2cd607
joeri <joerivanhurck@hotmail.be>
2023-02-24 16:47:11 +0100

    use javascript to build html

---
+++ index.js
@@ -0,0 +1,11 @@
+const titel = document.createElement("h1");
+titel.innerText = "Cote des Basques"
+const paragraph = document.createElement("p");
+paragraph.innerText = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti expedita odit quas aliquam incidunt reprehenderit nam fuga numquam officiis, possimus doloremque suscipit? Corrupti iure, voluptates obcaecati earum ea assumenda similique dolorem maiores hic dolor ipsam aliquid eum, cumque et velit accusamus modi vitae cupiditate quod consequatur ab debitis. Eius vel ullam commodi ducimus est quis a, impedit rerum optio quasi quidem. Ipsam animi dolor ratione omnis odio veritatis fugit, soluta culpa. Quam, repellendus. Quaerat ea illum dolore ipsam aspernatur iste eius pariatur animi quibusdam ducimus reiciendis esse quo maxime, facilis modi voluptas id repellendus autem itaque beatae dicta voluptatem, commodi quidem tempore. Minus earum dolore delectus ea! Tempora, assumenda! Quos, minima! Iste, cumque aperiam eius dolor, perspiciatis et quod illum nesciunt, quas animi dolores vero quae a unde recusandae veniam nam saepe odio. Impedit corporis iusto totam doloremque. Obcaecati facilis nam fugiat placeat debitis quisquam voluptatibus reprehenderit nihil dicta nesciunt aliquid temporibus eaque distinctio odit cum illum maxime ducimus, harum quis tempora sunt maiores sapiente? Quaerat illum odit nostrum esse voluptate hic delectus cupiditate quod distinctio ullam nam aliquam, dolorem asperiores deserunt praesentium dolore vel blanditiis assumenda. Cupiditate, atque fugiat corporis, dicta voluptates reprehenderit voluptas maxime nisi quas ab hic. "
+
+const body = document.querySelector("body");
+body.appendChild(titel);
+body.appendChild(paragraph)
+
+
+
