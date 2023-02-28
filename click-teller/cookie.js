dd95768bda336144ef2f9a1b8a39f84c192c921d
joeri <joerivanhurck@hotmail.be>
2023-02-28 21:41:56 +0100

    teller

---
+++ click-teller/cookie.js
@@ -0,0 +1,10 @@
+/*const clickbutton = document.querySelector(".bigcookie");
+clickbutton.click();
+clickbutton.addEventListener("click")
+
+
+let count = 0;
+
+if(clickbutton === "click"){
+    count++;
+}*/
