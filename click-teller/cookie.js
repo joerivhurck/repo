b8c8e60b89167a999254b4a406fb25b456f31b0a
joeri <joerivanhurck@hotmail.be>
2023-03-10 12:33:56 +0100

    functionality lock and unlock buttons

--- click-teller/cookie.js
+++ click-teller/cookie.js
@@ -1,9 +1,13 @@
 
 let count = 0;
+
 const cookieBtn = document.querySelector('.btnCookie');
 cookieBtn.addEventListener("click",clickCookie)
 
 const countCookie = document.querySelector(".counter");
+
+const cursorBtn = document.querySelector('.cursor');
+const omaBtn = document.querySelector('.oma');
 
 
 function clickCookie(){
@@ -11,29 +15,44 @@ function clickCookie(){
   countCookie.innerText = count;
 }
 
-const cursorBtn = document.querySelector('.cursor');
-
-
-const omaBtn = document.querySelector('.oma');
-
-
 function disable(){
   cursorBtn.disabled = true;
   omaBtn.disabled = true;
 }
 
-function enable(){
-  cursorBtn.disabled = false;
-  omaBtn.disabled = false;
+
+
+
+function powerLock(){
+  countCookie.addEventListener('DOMSubtreeModified',function(){
+    if(parseInt(countCookie.textContent,10) >= 10){
+      cursorBtn.disabled = false;
+    }else{
+      cursorBtn.disabled = true;
+    }
+  });
+  
+  countCookie.addEventListener('DOMSubtreeModified',function(){
+    if(parseInt(countCookie.textContent,10) >= 100){
+      omaBtn.disabled = false;
+    }else{
+      omaBtn.disabled = true;
+    }
+  });
 }
 
-
-
-if(countCookie.innerText === "10"){
-  enable();
-}else{
-  disable();
+function cursor(){
+  newCounter = count-=10;
+  countCookie.innerText = newCounter;
 }
+function oma(){
+  newCounter = count-=100;
+  countCookie.innerText = newCounter;
+}
+cursorBtn.addEventListener("click",cursor)
+omaBtn.addEventListener("click",oma)
 
 
+powerLock()
+disable()
 
